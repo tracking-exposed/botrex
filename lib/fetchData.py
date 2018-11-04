34e77938ee206b3c180d880b30abcb07cb893a30
berli0z <40333748+berli0z@users.noreply.github.com>
2018-11-04 17:39:17 +0100

    update fetchData.py
    implementing basic caching

--- lib/fetchData.py
+++ lib/fetchData.py
@@ -1,6 +1,9 @@
-# just for test purposes, needs caching.
 import requests
+from cachetools import cached, TTLCache
 
+cache = TTLCache(maxsize=1, ttl=600) # maxsize is 1 because we only fetch one element. time to live is 10 mins.
+
+@cached(cache)
 def getBasic():
     data = requests.get("https://facebook.tracking.exposed/api/v1/stats/basic/1").json()
     return data
