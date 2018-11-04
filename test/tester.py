eeed628cab47b7d02e7013f3d734c59c41f989a3
berli0z <40333748+berli0z@users.noreply.github.com>
2018-11-04 17:52:30 +0100

    tester.py update
    ready to call functions

--- test/tester.py
+++ test/tester.py
@@ -2,7 +2,5 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), "lib"))
 
 from createGraph import createGraph, timeSetting
+from createText import createText
 from fetchData import getBasic
-
-# try to call createGraph
-createGraph('impressions', getBasic(), timeSetting('week'))
