1406ed703c58b7a4eee11d04a2c0d30cebc0a2a1
berli0z <40333748+berli0z@users.noreply.github.com>
2018-11-05 12:28:28 +0100

    workaround for problems with cache

--- lib/fetchData.py
+++ lib/fetchData.py
@@ -1,7 +1,7 @@
 import requests
 from cachetools import cached, TTLCache
 
-cache = TTLCache(maxsize=1, ttl=600) # maxsize is 1 because we only fetch one element. time to live is 10 mins.
+cache = TTLCache(maxsize=100, ttl=600) # maxsize is 1 because we only fetch one element. time to live is 10 mins.
 
 @cached(cache)
 def getBasic():
